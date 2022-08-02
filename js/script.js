52d86777f3c316fbb81e15eb0e24aaa9d17c4588
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2022-08-03 01:11:40 +0300

    Added a function for the burger menu, and styles with animation for it

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// For burger-menu
+$('.header-menu__icon').click(function (event) {
+    $(this).toggleClass('active');
+    $('.header-menu').toggleClass('active');
+    if ($(this).hasClass('active')) {
+        $('body').data('scroll', $(window).scrollTop());
+    }
+    $('body').toggleClass('lock');
+    if (!$(this).hasClass('active')) {
+        $('body,html').scrollTop(parseInt($('body').data('scroll')));
+    }
+});
+
+//For future
+// var act = 'click';
+// if (isMobile.iOS()) {
+//     var act = 'touchstart';
+// }
+
+$('.wrapper').addClass('loaded');
