ab4309484458b48c14a4f9c9bc8f8436bf7e26d4
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2022-08-15 03:48:08 +0300

    Added block 4 and styles for him

--- js/script.js
+++ js/script.js
@@ -30,3 +30,50 @@ function ibg() {
     });
 }
 ibg();
+
+function map(n) {
+    google.maps.Map.prototype.setCenterWithOffset = function (
+        lating,
+        offsetX,
+        offsetY
+    ) {
+        var map = this;
+        var ov = new google.maps.OverlayView();
+        ov.onAdd = function () {
+            var proj = this.getProjection();
+            var aPoint = proj.fromLatLngToContainerPixel(lating);
+            aPoint.x = aPoint.x + offsetX;
+            aPoint.y = aPoint.y + offsetY;
+            map.panTo(proj.fromLatLngToContainerPixelToLatLng(aPoint));
+        };
+        ov.draw = function () {};
+        ov.setMap(this);
+    };
+    var markers = new Array();
+    var infowindow = new google.maps.Infowindow({});
+    var location = [[new google.maps.LatLng(53.819055, 27.8813694)]];
+    var options = {
+        zoom: 10,
+        panControl: false,
+        mapTypeControl: false,
+        center: location[0][0],
+        scrollwheel: false,
+        mapTypeId: google.maps.mapTypeId.ROADMAP,
+    };
+    var map = new google.maps.Map(document.getElementById('map'), options);
+    var icon = {
+        url: '/primeOne/img/1384.png',
+        scaledSize: new google.maps.Size(18, 20),
+        anchor: new google.maps.Point(9, 10),
+    };
+    for (var i = 0; i < location.length; i++) {
+        var marker = new google.maps.Marker({
+            position: location[i][0],
+            map: map,
+        });
+        markers.push(marker);
+    }
+}
+if ($('#map').length > 0) {
+    map();
+}
