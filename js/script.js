781520177490807b962828b0af68c61dc82d21a4
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2022-08-16 14:08:54 +0300

    Added google map, footer, fixed css

--- js/script.js
+++ js/script.js
@@ -11,12 +11,6 @@ $('.header-menu__icon').click(function (event) {
     }
 });
 
-//For future
-// var act = 'click';
-// if (isMobile.iOS()) {
-//     var act = 'touchstart';
-// }
-
 $('.wrapper').addClass('loaded');
 
 function ibg() {
@@ -31,49 +25,60 @@ function ibg() {
 }
 ibg();
 
-function map(n) {
-    google.maps.Map.prototype.setCenterWithOffset = function (
-        lating,
-        offsetX,
-        offsetY
-    ) {
-        var map = this;
-        var ov = new google.maps.OverlayView();
-        ov.onAdd = function () {
-            var proj = this.getProjection();
-            var aPoint = proj.fromLatLngToContainerPixel(lating);
-            aPoint.x = aPoint.x + offsetX;
-            aPoint.y = aPoint.y + offsetY;
-            map.panTo(proj.fromLatLngToContainerPixelToLatLng(aPoint));
-        };
-        ov.draw = function () {};
-        ov.setMap(this);
-    };
-    var markers = new Array();
-    var infowindow = new google.maps.Infowindow({});
-    var location = [[new google.maps.LatLng(53.819055, 27.8813694)]];
-    var options = {
-        zoom: 10,
-        panControl: false,
-        mapTypeControl: false,
-        center: location[0][0],
-        scrollwheel: false,
-        mapTypeId: google.maps.mapTypeId.ROADMAP,
-    };
-    var map = new google.maps.Map(document.getElementById('map'), options);
-    var icon = {
-        url: '/primeOne/img/1384.png',
-        scaledSize: new google.maps.Size(18, 20),
-        anchor: new google.maps.Point(9, 10),
-    };
-    for (var i = 0; i < location.length; i++) {
-        var marker = new google.maps.Marker({
-            position: location[i][0],
-            map: map,
-        });
-        markers.push(marker);
-    }
+// function map(n) {
+//     google.maps.Map.prototype.setCenterWithOffset = function (
+//         lating,
+//         offsetX,
+//         offsetY
+//     ) {
+//         var map = this;
+//         var ov = new google.maps.OverlayView();
+//         ov.onAdd = function () {
+//             var proj = this.getProjection();
+//             var aPoint = proj.fromLatLngToContainerPixel(lating);
+//             aPoint.x = aPoint.x + offsetX;
+//             aPoint.y = aPoint.y + offsetY;
+//             map.panTo(proj.fromLatLngToContainerPixelToLatLng(aPoint));
+//         };
+//         ov.draw = function () {};
+//         ov.setMap(this);
+//     };
+//     var markers = new Array();
+//     var infowindow = new google.maps.Infowindow({});
+//     var location = [[new google.maps.LatLng(53.819055, 27.8813694)]];
+//     var options = {
+//         zoom: 10,
+//         panControl: false,
+//         mapTypeControl: false,
+//         center: location[0][0],
+//         scrollwheel: false,
+//         mapTypeId: google.maps.mapTypeId.ROADMAP,
+//     };
+//     var map = new google.maps.Map(document.getElementById('map'), options);
+//     var icon = {
+//         url: '/primeOne/img/1384.png',
+//         scaledSize: new google.maps.Size(18, 20),
+//         anchor: new google.maps.Point(9, 10),
+//     };
+//     for (var i = 0; i < location.length; i++) {
+//         var marker = new google.maps.Marker({
+//             position: location[i][0],
+//             map: map,
+//         });
+//         markers.push(marker);
+//     }
+// }
+// if ($('#map').length > 0) {
+//     map();
+// }
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
 }
-if ($('#map').length > 0) {
-    map();
-}
+
+window.initMap = initMap;
